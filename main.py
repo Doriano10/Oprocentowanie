4e83189746acd58e25b42b6771c40d1707ab4e7f
Dorian Tissot <dorian.tissot@protonmail.com>
2023-04-20 17:02:59 +0200

    add input taking lending rate from user

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("Witaj w kalkulatorze oprocentowania pożyczki")
 
 # pobranie od użytkownika wartości początkowej kredytu
 kwota_początkowa = float(input("Proszę podać początkową kwotę kredytu:\n"))
+
+# pobranie od użytkownika wartości oprocentowania pożyczki
+float(input("Proszę podać oprocentowanie pożyczki:\n"))
