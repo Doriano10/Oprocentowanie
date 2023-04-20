87fea0a617b4476190cf09f97348aacab30444d0
Dorian Tissot <dorian.tissot@protonmail.com>
2023-04-20 16:59:38 +0200

    add input taking credit value from user

---
+++ main.py
@@ -0,0 +1,5 @@
+# wiadomość powitalna
+print("Witaj w kalkulatorze oprocentowania pożyczki")
+
+# pobranie od użytkownika wartości początkowej kredytu
+kwota_początkowa = float(input("Proszę podać początkową kwotę kredytu:\n"))
