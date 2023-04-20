3ede6962a7c5f7b0ec991e18671ce4b40f1b41b7
Dorian Tissot <dorian.tissot@protonmail.com>
2023-04-20 17:18:20 +0200

    add variables with inflation rates for 24 months

--- main.py
+++ main.py
@@ -9,3 +9,29 @@ oprocentowanie = float(input("Proszę podać oprocentowanie pożyczki:\n"))
 
 # pobranie od użytkownika wartości kwoty raty stałej
 rata = float(input("Proszę podać wartość raty kredytu:\n"))
+
+# zmienne zawierające wartość inflacji w następnych miesiącach
+infl_1 = 1.59282448436825
+infl_2 = -0.453509101198007
+infl_3 = 2.32467171712441
+infl_4 = 1.26125440724877
+infl_5 = 1.78252628571251
+infl_6 = 2.32938454145522
+infl_7 = 1.50222984223283
+infl_8 = 1.78252628571251
+infl_9 = 2.32884899407637
+infl_10 = 0.616921348207244
+infl_11 = 2.35229588637833
+infl_12 = 0.337779545187098
+infl_13 = 1.57703524727525
+infl_14 = -0.292781442607648
+infl_15 = 2.48619659017508
+infl_16 = 0.267110317834564
+infl_17 = 1.41795267229799
+infl_18 = 1.05424326726375
+infl_19 = 1.4805201044812
+infl_20 = 1.57703524727525
+infl_21 = -0.0774206903147018
+infl_22 = 1.16573339872354
+infl_23 = -0.404186717638335
+infl_24 = 1.49970852083123
